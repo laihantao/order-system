36d9e3e24ae3a87892c2a04ad7930ca78fc9e7fa
Lai Han Tao <hantao.lai@coway.com.my>
2026-04-13 11:18:21 +0800

    Initial commit

--- src/main/java/com/example/order_system/OrderSystemApplication.java
+++ src/main/java/com/example/order_system/OrderSystemApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class OrderSystemApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OrderSystemApplication.class, args);
+		SpringApplication.run(OrderSystemApplication.class, args); 
 	}
 
 }
